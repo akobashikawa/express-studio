ff9cd00004e7376a44b814548bfd9cec90c59870
Antonio Kobashikawa <akobashikawa@gmail.com>
2022-10-06 10:41:29 -0500

    Agrega script dev

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 
+const PORT = 3000
+
 app.get('', (req, res) => {
     res.send('Express Studio');
 });
 
-app.listen(3000, () => {
-    console.log('ExpressJS server running...');
+app.listen(PORT, () => {
+    console.log(`ExpressJS server running on port ${PORT}...`);
 });
